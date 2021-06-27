210c057c8d9b8e7fe45bb8242267fe4cfe75ebd2
John Labod <jl@labod.co>
2021-06-27 11:42:32 -0400

    fix speech so it doesn't sound so demonic

--- actions/speech.py
+++ actions/speech.py
@@ -3,10 +3,8 @@ from espeakng import ESpeakNG
 def say(phrase):
     print(phrase)
     esng = ESpeakNG()
-    esng.voice = "en-us"
-    esng.pitch = 40
-    esng.speed = 50
-    esng.volume = 200
-    esng.voices
-    esng.say(phrase)
-
+    esng.voice = "en+f4"
+    esng.speed = 150
+    esng.word_gap = 30
+    # the k is because the start of the phrase is cut off
+    esng.say("k " + phrase)
